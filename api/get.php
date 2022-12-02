c2fefd542a77bc96e2758a2fc2077bc48f99c446
jvalentfuentezuelas <90899095+jvalentfuentezuelas@users.noreply.github.com>
2022-12-02 18:35:42 +0100

    loadbalncer consuming api rest

---
+++ api/get.php
@@ -0,0 +1,2 @@
+<?php
+echo json_encode(['nombre'=> 'manolo','edad'=> '54']);
--- public/index.php
+++ public/index.php
@@ -1,3 +1,22 @@
 <?php
+use GuzzleHttp\Client;
 echo "Dirección del Servidor ==> ".$_SERVER["SERVER_ADDR"]."<br>";
 echo "Nombre del Servidor ==> ".$_SERVER['SERVER_NAME']."<br>";
+require "./vendor/autoload.php";
+
+//Crear el cliente para llamadas al servicio
+//Debes cambiar el valor de base_uri a la direcciÃ³n en donde esta tu servicio
+//El valor de timeout, en este caso es para decir que despues de 5 segundos
+//si el servicio no responde, se cancela el proceso.
+$client = new Client([
+    'base_uri' => 'http://api/get.php',
+    'timeout'  => 5.0,
+]);
+//Hacer la llamada al metodo get, sin ningÃºn parametro
+$res = $client->request('GET');
+if ($res->getStatusCode() == '200') //Verifico que me retorne 200 = OK
+{
+  $obj = json_decode($res->getBody());
+  echo $obj->nombre;
+}
+?>
